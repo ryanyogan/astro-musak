2e0d2027a80a339c42379bdbbfbaf27ae511b248
Ryan Yogan <ryanyogan@gmail.com>
2023-09-01 14:28:40 -0500

    feat // whoops, vercel ryan, not cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  adapter: cloudflare(),
-  // experimental: {
-  //   viewTransitions: true,
-  // },
+  adapter: vercel(),
 });
