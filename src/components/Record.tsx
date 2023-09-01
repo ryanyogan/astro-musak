564430793249f7e624ba2610c12a12582bca9670
Ryan Yogan <ryanyogan@gmail.com>
2023-09-01 14:47:01 -0500

    feat // okay astro is getting realy fun

---
+++ src/components/Record.tsx
@@ -0,0 +1,36 @@
+import { currentTrack, isPlaying } from "src/state";
+
+interface RecordProps {
+  albumId: string;
+  title: string;
+  imageUrl: string;
+}
+
+export default function Record({ albumId, title, imageUrl }: RecordProps) {
+  const isPlayingCurrentRecord =
+    isPlaying.value && currentTrack.value.albumId === albumId;
+
+  const className = `absolute top-0 opacity-0 vynil-imge vynil-animation-in ${
+    isPlayingCurrentRecord && "-spinning"
+  }`;
+
+  return (
+    <div class="relative shadow-xl mr-32 w-72 md:w-auto">
+      <img
+        src={imageUrl}
+        alt={title}
+        width="400"
+        height="400"
+        class="block rounded-md tag-album-cover relative z-10 bg-white"
+        style={`view-transition-name: record-${albumId}`}
+      />
+      <img
+        src="/vynil-lp.webp"
+        width="400"
+        height="400"
+        class={className}
+        style={`view-transition-name: vinyl-${albumId};`}
+      />
+    </div>
+  );
+}
